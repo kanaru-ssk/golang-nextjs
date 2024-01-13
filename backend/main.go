2c78a1e6242eadf81d92044ae1731248092488d7
kanaru-ssk <2019january.0131@gmail.com>
2024-01-13 20:59:52 +0900

    add postgres

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		user := User{
-			Name: "Kanaru Sasaki",
-			Age:  23,
+		db, err := sql.Open("postgres", "postgres://postgres:pass@db:5432/users?sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		json.NewEncoder(w).Encode(user)
+		var users []User
+		for rows.Next() {
+			m := User{}
+			rows.Scan(&m.Id, &m.Name, &m.Email, &m.Age)
+			log.Print("test : ", m)
+			users = append(users, m)
+		}
+
+		json.NewEncoder(w).Encode(users)
 	})
 
 	http.ListenAndServe(":8080", nil)
