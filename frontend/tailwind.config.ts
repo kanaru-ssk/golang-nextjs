6fc8b221234c56b5f3faf256c9cffd927eb2a9a4
kanaru-ssk <2019january.0131@gmail.com>
2024-03-02 10:53:08 +0900

    tailwindがfeatures/配下にも適用されるよう修正

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 };
 export default config;
